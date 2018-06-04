35a1705a102209f62ba9e3f620461326bfc04eb2
Lockal <lockal333@gmail.com>
2018-06-04 13:17:22 +0300

    test

---
+++ main.js
@@ -0,0 +1,6 @@
+const acc = document.querySelector('.upper__lang-select');
+
+const handleClick = () => document.querySelector('.upper__lang-select-list').style.display = "flex";
+
+acc.addEventListener('click', handleClick);
+
