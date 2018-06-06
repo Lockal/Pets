c06ce0733bf6c0baede8742e85e315764cca295d
Lockal <lockal333@gmail.com>
2018-06-06 07:59:55 +0300

    m-menu

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-const acc = document.querySelector('.upper__lang-select');
+const acc = document.querySelector('.m-header__menu'); {
 
-const handleClick = () => document.querySelector('.upper__lang-select-list').style.display = "flex";
+const handleClick = () => document.querySelector('.m-header__menu-block').style.display = "flex";
 
 acc.addEventListener('click', handleClick);
+};
+
+const acc2 = document.querySelector('.m-menu__close'); { 
+
+const handleClick = () => document.querySelector('.m-header__menu-block').style.display = "none";
+
+acc2.addEventListener('click', handleClick);
+};
+
+const acc3 = document.querySelector('.m-menu__element:nth-child(3)'); { 
+
+    const handleClick = () => document.querySelector('.m-menu__submenu').style.display = "flex";
+    
+    acc3.addEventListener('click', handleClick);
+};
+
+const acc4 = document.querySelector('.back'); { 
+
+    const handleClick = () => document.querySelector('.m-menu__submenu').style.display = "flex";
+    
+   acc4.addEventListener('click', handleClick);
+};
 
